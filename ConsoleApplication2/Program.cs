bf893609e1532344ea42ab197a966f835b9f2555
Sandra <Sandra@DESKTOP-OEC3L74>
2016-09-20 11:36:17 +0100

    Add project files.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //A string is a class, which can be instantiated like this
+        string s = "Hi there";
+        Console.WriteLine(s);//Hi there
+        ChangeString(s);
+        Console.WriteLine(s);//Hi there
+        ChangeString(ref s);
+        Console.WriteLine(s);//s is now a null reference
+    }
+
+    static void ChangeString(string str)
+    {
+        str = null;
+    }
+
+    static void ChangeString(ref string str)
+    {
+        str = null;
+    }
+}
